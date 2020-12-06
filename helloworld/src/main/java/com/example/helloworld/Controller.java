eadb4de95c6169f949d32b58fd406d0fe0ec5463
RonaldLin98 <1746090950@qq.com>
2020-12-06 23:49:32 +0800

    new test

--- helloworld/src/main/java/com/example/helloworld/Controller.java
+++ helloworld/src/main/java/com/example/helloworld/Controller.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 import java.util.Date;
 public class Controller {
+    int a;
     Date date = new Date();
     public String hello(String name){
         return name + "，<font color='red'>您好：</font>" + date.getTime();
